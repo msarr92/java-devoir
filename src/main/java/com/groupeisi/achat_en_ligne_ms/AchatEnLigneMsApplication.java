67def8629b65aa920da26ff6a0590131ab8500b0
Mouha sarr <msarr091@gmail.com>
2026-03-14 14:57:14 +0000

    first commit

---
+++ src/main/java/com/groupeisi/achat_en_ligne_ms/AchatEnLigneMsApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.achat_en_ligne_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchatEnLigneMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchatEnLigneMsApplication.class, args);
+	}
+
+}
